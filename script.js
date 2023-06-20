6906a04fe9c43a09bbd517975b6130998a429d4a
NSA-ict <121605157+NSA-ict@users.noreply.github.com>
2023-06-20 11:26:54 +0630

    script.js

--- script.js
+++ script.js
@@ -1,214 +1,79 @@
-// Client ID and API key from the Developer Console
-var CLIENT_ID = '177292398339-8asgcp03jgug3sceo6e0i4artk7qekt4.apps.googleusercontent.com';
-var API_KEY = 'AIzaSyBGP0su_B0on_PmPEVLBTSOxur6FdaNCFY';
+// Load the Google Sheets API
+function initClient() {
+    gapi.client.init({
+        apiKey: 'AIzaSyBGP0su_B0on_PmPEVLBTSOxur6FdaNCFY',
+        discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+    }).then(function () {
+        // Fetch data from the Google Sheet
+        fetchData();
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
 
-// ID of the Google Sheet
-var sheetId = '1C7O_xWAzbpffYQfT0su_Sm9876HLWXN0xknnxsK5mu8';
+// Fetch data from the Google Sheet
+function fetchData() {
+    gapi.client.sheets.spreadsheets.values.get({
+        spreadsheetId: '1C7O_xWAzbpffYQfT0su_Sm9876HLWXN0xknnxsK5mu8',
+        range: 'Sheet1!A2:C', // Adjust the range as per your sheet
+    }).then(function (response) {
+        var values = response.result.values;
+        if (values && values.length > 0) {
+            console.log(values); // You can remove this line or modify it to suit your needs
+            // Store the data in a variable for later use
+            window.data = values;
+        } else {
+            console.log('No data found.');
+        }
+    }).catch(function (error) {
+        console.log(error);
+    });
+}
 
-// Column index for data search (column index starts from 0)
-var searchColumnIndex = 8; // Column 9 (index 8)
+// Process and display the search results
+function processData() {
+    var searchValue = document.getElementById('searchInput').value.toLowerCase();
+    var outputDiv = document.getElementById('output');
+    outputDiv.innerHTML = '';
+
+    var matchFound = false;
+
+    if (window.data && window.data.length > 0) {
+        for (var i = 0; i < window.data.length; i++) {
+            var item = window.data[i];
+            var name = item[0].toLowerCase();
+            var info = item[1];
+            var photoUrl = item[2];
+
+            if (name.includes(searchValue)) {
+                var card = document.createElement('div');
+                card.className = 'card';
+
+                var img = document.createElement('img');
+                img.src = photoUrl;
+                img.alt = name + ' photo';
+                card.appendChild(img);
+
+                var nameElement = document.createElement('h2');
+                nameElement.textContent = name;
+                card.appendChild(nameElement);
+
+                var infoElement = document.createElement('p');
+                infoElement.textContent = info;
+                card.appendChild(infoElement);
+
+                outputDiv.appendChild(card);
+                matchFound = true;
+            }
+        }
+    }
+
+    if (!matchFound) {
+        var noMatchMsg = document.createElement('p');
+        noMatchMsg.textContent = 'No matching data found.';
+        outputDiv.appendChild(noMatchMsg);
+    }
+}
 
 // Load the Google Sheets API
 gapi.load('client', initClient);
-
-// Initialize the API client
-function initClient() {
-  gapi.client.init({
-    apiKey: API_KEY,
-    clientId: CLIENT_ID,
-    discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-  }).then(function () {
-    // API client is initialized, load the sheet data
-    loadSheetData();
-  }, function (error) {
-    console.log('Error initializing Google Sheets API client:', error);
-  });
-}
-
-// Load the data from the Google Sheet
-function loadSheetData() {
-  gapi.client.sheets.spreadsheets.values.get({
-    spreadsheetId: sheetId,
-    range: 'Sheet1' // Change the sheet name if needed
-  }).then(function (response) {
-    var sheetData = response.result.values;
-    console.log('Sheet data loaded:', sheetData);
-
-    // Populate the hidden form with Google Sheet data
-    var hiddenForm = document.getElementById('hiddenForm');
-    for (var i = 0; i < sheetData.length; i++) {
-      var rowData = sheetData[i];
-      if (rowData.length === 9) { // Check if there are 9 columns of data
-        var searchValue = rowData[searchColumnIndex];
-        var nameField = document.createElement('input');
-        nameField.type = 'hidden';
-        nameField.name = 'name_' + i;
-        nameField.value = searchValue;
-        hiddenForm.appendChild(nameField);
-      }
-    }
-  }, function (error) {
-    console.log('Error loading Google Sheet data:', error);
-  });
-}
-
-// Search data based on the entered value
-function searchData() {
-  var searchValue = document.getElementById('searchInput').value.trim();
-  var hiddenForm = document.getElementById('hiddenForm');
-  var searchResults = [];
-
-  for (var i = 0; i < hiddenForm.elements.length; i++) {
-    var nameField = hiddenForm.elements['name_' + i];
-
-    if (nameField.value === searchValue) {
-      var result = {
-        name: nameField.value
-      };
-      searchResults.push(result);
-    }
-  }
-
-  displaySearchResults(searchResults);
-}
-
-// Display the search results
-function displaySearchResults(results) {
-  var resultsContainer = document.getElementById('searchResults');
-  resultsContainer.innerHTML = '';
-
-  if (results.length === 0) {
-    resultsContainer.innerText = 'No results found.';
-    return;
-  }
-
-  for (var i = 0; i < results.length; i++) {
-    var result = results[i];
-    var card = document.createElement('div');
-    card.classList.add('card');
-
-    var photo = document.createElement('div');
-    photo.classList.add('photo');
-    var image = document.createElement('img');
-    image.src = 'student_photo.jpg'; // Replace with the path to the student photo
-    photo.appendChild(image);
-
-    var details = document.createElement('div');
-    details.classList.add('details');
-    var nameElement = document.createElement('p');
-    nameElement.classList.add('name');
-    nameElement.innerText = 'Name: ' + result.name;
-    details.appendChild(nameElement);
-
-    card.appendChild(photo);
-    card.appendChild(details);
-    resultsContainer.appendChild(card);
-  }
-}
-// Client ID and API key from the Developer Console
-var CLIENT_ID = '177292398339-8asgcp03jgug3sceo6e0i4artk7qekt4.apps.googleusercontent.com';
-var API_KEY = 'AIzaSyBGP0su_B0on_PmPEVLBTSOxur6FdaNCFY';
-
-// ID of the Google Sheet
-var sheetId = '1C7O_xWAzbpffYQfT0su_Sm9876HLWXN0xknnxsK5mu8';
-
-// Column index for data search (column index starts from 0)
-var searchColumnIndex = 8; // Column 9 (index 8)
-
-// Load the Google Sheets API
-gapi.load('client', initClient);
-
-// Initialize the API client
-function initClient() {
-  gapi.client.init({
-    apiKey: API_KEY,
-    clientId: CLIENT_ID,
-    discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
-  }).then(function () {
-    // API client is initialized, load the sheet data
-    loadSheetData();
-  }, function (error) {
-    console.log('Error initializing Google Sheets API client:', error);
-  });
-}
-
-// Load the data from the Google Sheet
-function loadSheetData() {
-  gapi.client.sheets.spreadsheets.values.get({
-    spreadsheetId: sheetId,
-    range: 'Sheet1' // Change the sheet name if needed
-  }).then(function (response) {
-    var sheetData = response.result.values;
-    console.log('Sheet data loaded:', sheetData);
-
-    // Populate the hidden form with Google Sheet data
-    var hiddenForm = document.getElementById('hiddenForm');
-    for (var i = 0; i < sheetData.length; i++) {
-      var rowData = sheetData[i];
-      if (rowData.length === 9) { // Check if there are 9 columns of data
-        var searchValue = rowData[searchColumnIndex];
-        var nameField = document.createElement('input');
-        nameField.type = 'hidden';
-        nameField.name = 'name_' + i;
-        nameField.value = searchValue;
-        hiddenForm.appendChild(nameField);
-      }
-    }
-  }, function (error) {
-    console.log('Error loading Google Sheet data:', error);
-  });
-}
-
-// Search data based on the entered value
-function searchData() {
-  var searchValue = document.getElementById('searchInput').value.trim();
-  var hiddenForm = document.getElementById('hiddenForm');
-  var searchResults = [];
-
-  for (var i = 0; i < hiddenForm.elements.length; i++) {
-    var nameField = hiddenForm.elements['name_' + i];
-
-    if (nameField.value === searchValue) {
-      var result = {
-        name: nameField.value
-      };
-      searchResults.push(result);
-    }
-  }
-
-  displaySearchResults(searchResults);
-}
-
-// Display the search results
-function displaySearchResults(results) {
-  var resultsContainer = document.getElementById('searchResults');
-  resultsContainer.innerHTML = '';
-
-  if (results.length === 0) {
-    resultsContainer.innerText = 'No results found.';
-    return;
-  }
-
-  for (var i = 0; i < results.length; i++) {
-    var result = results[i];
-    var card = document.createElement('div');
-    card.classList.add('card');
-
-    var photo = document.createElement('div');
-    photo.classList.add('photo');
-    var image = document.createElement('img');
-    image.src = 'student_photo.jpg'; // Replace with the path to the student photo
-    photo.appendChild(image);
-
-    var details = document.createElement('div');
-    details.classList.add('details');
-    var nameElement = document.createElement('p');
-    nameElement.classList.add('name');
-    nameElement.innerText = 'Name: ' + result.name;
-    details.appendChild(nameElement);
-
-    card.appendChild(photo);
-    card.appendChild(details);
-    resultsContainer.appendChild(card);
-  }
-}
